9920d13da01e264dbc308d331bbc5c4d2e9bd7a9
abhiajk <31100400+abhiajk@users.noreply.github.com>
2017-08-23 19:03:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article-one',function(req,res){
      res.sendFile(path.join(__dirname, 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
-    res.send("article two requested and served");
+        res.sendFile(path.join(__dirname, 'article-two.html')); 
 });
 app.get('/article-three',function(req,res){
-    res.send("article three requested and served");
+     res.sendFile(path.join(__dirname, 'article-three.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
