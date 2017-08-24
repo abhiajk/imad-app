2fd6c9f2f661f9c8acfaeed26883ac765dcf4db0
abhiajk <31100400+abhiajk@users.noreply.github.com>
2017-08-24 19:13:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,7 @@ var element=document.getElementById("main-text");
 element.innerHTML="new value";
 var img=document.getElementById('pic');
 img.onclick = function(){
-    img.style.marginLeft="180px";
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+'px';
+    setInterval(moveRight,100);
 };
