f0a07006be3dfb45b13481f0ffe5084a34a03e49
abhiajk <31100400+abhiajk@users.noreply.github.com>
2017-08-24 19:14:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,7 @@ console.log('Loaded!');
 var element=document.getElementById("main-text");
 element.innerHTML="new value";
 var img=document.getElementById('pic');
+var marginLeft=0;
 img.onclick = function(){
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
