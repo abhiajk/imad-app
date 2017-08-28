f8d0bcfec2a7af326d8124e83ae23ff1472616ce
abhiajk <31100400+abhiajk@users.noreply.github.com>
2017-08-28 15:34:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,3 @@
-var button=document.getElementById("counter");
-button.onclick = function () {
-  var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function(){
-    if(request.readyState === XMLHttpRequest.DONE)  {
-        if(request.status === 200){
-            var counter = request.responseText;
-             var span   = document.getElementById("count");
-             span.innerHTML = counter.toString();
-        }
-    }  
-    };
-   request.open('GET','http://jabhijithabhi.imad.hasura-app.io/counter',true);
-   request.send(null);
-};
 
 var submit=document.getElementById("submit_btn");
 submit.onclick=function(){
@@ -23,14 +7,18 @@ var Name=nameinput.value;
     request.onreadystatechange = function(){
     if(request.readyState === XMLHttpRequest.DONE)  {
         if(request.status === 200){
-     var names=request.responseText;
-     names=JSON.parse(names);
-    var list='';
-    for(var i=0;i<names.length;i++){
-   list+='<li>'+ names[i]+'</li>' ;}
-   var ul=document.getElementById("namelist");
-   ul.innerHTML=list;
-  }}}; 
-   request.open('GET','http://jabhijithabhi.imad.hasura-app.io/submit-name?name='+Name,true);
-   request.send(null);  
+    console.log('user logged in');
+    alert('logged in succesfully');}
+    else if(request.status===403){
+        alert('username/password is incorrect');
+    }else if(request.status===500){
+        alert('something went wrong');
+    }
+    }
+    }; 
+  var username =document.getElementById("username").value;
+    var password =document.getElementById("password").value;
+   request.open('POST','http://jabhijithabhi.imad.hasura-app.io/login',true);
+   request.setRequestHeader('Content-Type','application/json');
+   request.send(JSON.stringify({username:username,password:password}));  
 };
